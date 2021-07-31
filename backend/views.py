39e0cb9446a2df6191e729f9777f88ae728dac8b
Shivam-Singh <sd.shivam.00@gmail.com>
2021-07-31 16:52:31 +0530

    Update views.py

--- backend/views.py
+++ backend/views.py
@@ -1,6 +1,6 @@
 from django import http
 from django.shortcuts import redirect, render
-from .models import *
+from .models import files
 from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponse
 # Create your views here.
