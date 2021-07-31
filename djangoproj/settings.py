38f2050c55da12924731cf7c9c4497e53393c81f
Shivam-Singh <sd.shivam.00@gmail.com>
2021-07-31 16:03:53 +0530

    Update settings.py

--- djangoproj/settings.py
+++ djangoproj/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '84gvxwpw6=te^ubkgc05%6&5i9@#6^xc)l4bk68l@era8k+**0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sd-share-zone.herokuapp.com]
 
 
 # Application definition
